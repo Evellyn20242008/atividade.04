06556136bb371bcedf4f9486aeb5543f07915d91
Evellyn20242008 <Evellynmartins579@gmail.com>
2024-09-05 09:56:20 -0300

    media

---
+++ ativ04.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+nota1=int(input("digite a nota1"))
+nota02=int(input("digite a nota2"))         
+nota03=int(input("digitea nota3"))       
+
+media=(nota1+nota02+nota03) /3
+
+ 
+
+print(f"a soma da media é = {media}")
